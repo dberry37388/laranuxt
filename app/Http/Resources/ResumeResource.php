62d6110bfc7605daf9166865e82c451ac80d341f
Daniel Berry <daniel@jstreetdigital.com>
2022-08-05 06:32:03 -0500

    add resume controller, requests, and resource
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

---
+++ app/Http/Resources/ResumeResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResumeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'location' => $this->location,
+            'job_title' => $this->,job_title,
+            'portfolio_url' => $this->,portfolio_url,
+            'phone' => $this->phone,
+        ];
+    }
+}
