2dc5a66f3cfc9f5ad04d2ce5d48d0e7a3dcaccf5
Daniel Berry <daniel@jstreetdigital.com>
2022-08-07 12:59:17 -0500

    add experience relation to resume and experience model test
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

--- app/Models/Resume.php
+++ app/Models/Resume.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Resume extends Model
 {
@@ -22,5 +23,10 @@ class Resume extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function experiences(): HasMany
+    {
+        return $this->hasMany(Experience::class);
     }
 }
